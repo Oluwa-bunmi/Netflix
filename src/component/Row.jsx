bcfd5298322e341ec1eba2c2414f551f286ea70b
Jummy007 <gbemisoyeadejumoke@gmail.com>
2023-04-24 02:04:24 +0100

    arrow icons for slides showing

--- src/component/Row.jsx
+++ src/component/Row.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
@@ -14,7 +15,11 @@ const Row = ({ title, fetchURL }) => {
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          size={40}
+          className="bg-white rounded-full absolute left-0 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
         <div
           id={"slider"}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
@@ -23,6 +28,10 @@ const Row = ({ title, fetchURL }) => {
             <Movie key={id} item={item} />
           ))}
         </div>
+        <MdChevronRight
+          size={40}
+          className="bg-white rounded-full absolute right-0 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
       </div>
     </>
   );
