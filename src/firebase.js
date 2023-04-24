12f47113d27f271cb1b13089979a23b5e28e8bbc
Jummy007 <gbemisoyeadejumoke@gmail.com>
2023-04-24 02:23:48 +0100

    starting with backend firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEFa2CFjkkqlL1V72RiLnioliZDQAnAIM",
+  authDomain: "netflix-clone-8ad32.firebaseapp.com",
+  projectId: "netflix-clone-8ad32",
+  storageBucket: "netflix-clone-8ad32.appspot.com",
+  messagingSenderId: "91648874608",
+  appId: "1:91648874608:web:8549544e58e4ef80713cb5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
