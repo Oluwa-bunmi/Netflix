76392b06d799d2c283eb71ebe587d61560865afd
Jummy007 <gbemisoyeadejumoke@gmail.com>
2023-04-24 01:27:11 +0100

    title showing on row overlay

--- src/component/Row.jsx
+++ src/component/Row.jsx
@@ -15,11 +15,17 @@ const Row = ({ title, fetchURL }) => {
       <div className="relative flex items-center">
         <div id={"slider"}>
           {movies.map((item, id) => (
-            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relativep-2">
+            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
               <img
+                className="w-full h-auto block"
                 src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
                 alt={item?.title}
               />
+              <div className="absolute top-0 left-0 w-full h-full opacity-0 text-white hover:bg-black/80 hover:opacity-100">
+                <p className="text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+                  {item?.title}
+                </p>
+              </div>
             </div>
           ))}
         </div>
