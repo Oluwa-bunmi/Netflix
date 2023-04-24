01c7c5e0998c70b78eadbe40d7ba5e8d65e2cc0c
Jummy007 <gbemisoyeadejumoke@gmail.com>
2023-04-24 01:13:17 +0100

    upcoming, toprated etc images displayed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import Main from "../component/Main";
-
+import Row from "../component/Row";
+import requests from "../Requests";
 const Home = () => {
     return ( 
         <div>
             <Main/>
+            <Row title="UpComing" fetchURL={requests.requestUpcoming}/>
+            <Row title="Popular" fetchURL={requests.requestPopular}/>
+            <Row title="Trending" fetchURL={requests.requestTrending}/>
+            <Row title="Top Rated" fetchURL={requests.requestTopRated}/>
+            <Row title="Horror" fetchURL={requests.requestHorror}/>
         </div>
      );
 }
