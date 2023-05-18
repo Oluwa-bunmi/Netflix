c6f2eec5892e719af47456632dee46fc3c2daa16
Jummy007 <gbemisoyeadejumoke@gmail.com>
2023-05-18 16:55:08 +0100

    done coding

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,14 +10,14 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER,
   appId: import.meta.env.VITE_APP_ID,
 };
 // const MY_VARIABLE = import.meta.env.VITE_MY_VARIABLE;
 
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,5 +1,24 @@
+import bg from "../assets/netflix.jpg";
+import SavedMovies from "../component/SavedMovies";
+
 const Account = () => {
-    return ( <div>acc</div> );
-}
- 
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          src={bg}
+          alt="/"
+          className="h-[400px] w-full object-cover"
+        />
+
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+      <SavedMovies />
+    </>
+  );
+};
+
 export default Account;
